a6e771467e4fac4c36b19f33916bec6cceae652e
Nikit Singh <nikitskulhari@gmail.com>
2024-01-04 22:37:56 +0530

    Progress: 29

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,81 @@
-use std::io::{stdin, Write};
-use std::io::{self, Error, Read};
-use std::os::fd::AsRawFd;
+use std::io::{self, Error, Read, Write};
+use std::os::fd::{AsRawFd, RawFd};
 use std::process::exit;
 
 use termios::*;
 
-// #define CTRL_KEY(k) ((k) & 0x1f)
+struct EditorConfig {
+    screenrows: u16,
+    screencols: u16,
+    term: Termios,
+    fd: RawFd,
+}
+
+impl Default for EditorConfig {
+    fn default() -> Self {
+        let fd = io::stdin().as_raw_fd();
+        let mut raw: Termios = Termios::from_fd(fd).unwrap();
+        tcgetattr(fd, &mut raw).unwrap();
+
+        let (screenrows, screencols) = get_window_size().unwrap();
+
+        EditorConfig {
+            screenrows,
+            screencols,
+            term: raw,
+            fd,
+        }
+    }
+}
+
+fn get_window_size() -> Option<(u16, u16)> {
+    let mut winsize = libc::winsize {
+        ws_row: 0,
+        ws_col: 0,
+        ws_xpixel: 0,
+        ws_ypixel: 0,
+    };
+
+    let mut out = io::stdout();
+
+    unsafe {
+        // if libc::ioctl(out.as_raw_fd(), libc::TIOCGWINSZ, &mut winsize) == -1 {
+            if out.write(b"\x1b[999C\x1b[999B").unwrap() != 12 {
+                return None;
+            }
+            editor_read_key();
+        // }
+    }
+
+    Some((winsize.ws_row, winsize.ws_col))
+}
+
 fn ctrl_key(k: char) -> u8 {
     k as u8 & 0x1f //00011111
 }
 
-fn enable_raw_mode() -> Result<Termios, Error> {
-    let fd = stdin().as_raw_fd();
-    let mut raw = Termios::from_fd(fd)?;
-    let orig_raw = raw;
-
-    tcgetattr(fd, &mut raw)?;
+fn enable_raw_mode(cfg: &EditorConfig) -> Result<(), Error> {
+    let mut raw = cfg.term;
+    tcgetattr(cfg.fd, &mut raw)?;
     raw.c_iflag &= !(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_cflag |= CS8;
     raw.c_oflag &= !(OPOST);
     raw.c_lflag &= !(ECHO | ICANON | ISIG | IEXTEN);
     raw.c_cc[VMIN] = 0;
     raw.c_cc[VTIME] = 1;
-    tcsetattr(fd, TCSAFLUSH, &mut raw)?;
+    tcsetattr(cfg.fd, TCSAFLUSH, &mut raw)?;
 
-    Ok(orig_raw)
+    Ok(())
 }
 
 fn disable_raw_mod(raw: &Termios) -> Result<(), Error> {
     let mut raw = raw;
-    tcsetattr(stdin().as_raw_fd(), TCSAFLUSH, &mut raw)?;
+    tcsetattr(io::stdin().as_raw_fd(), TCSAFLUSH, &mut raw)?;
     Ok(())
 }
 
 fn editor_read_key() -> char {
-    let byte = stdin().by_ref().bytes().next();
+    let byte = io::stdin().by_ref().bytes().next();
 
     match byte {
         Some(ch) => ch.ok().unwrap() as char,
@@ -42,31 +83,49 @@ fn editor_read_key() -> char {
     }
 }
 
-fn editor_process_key(raw: &Termios) {
+fn editor_process_key(cfg: &EditorConfig) {
     let c = editor_read_key();
     let ctrl_q = ctrl_key('q');
 
     match c as u8 {
         x if x == ctrl_q => {
-            editor_refresh_screen();
-            disable_raw_mod(&raw).unwrap();
+            term_refresh();
+            disable_raw_mod(&cfg.term).unwrap();
             exit(0);
-        },
+        }
         d => println!("{}", d as char),
     }
 }
 
-fn editor_refresh_screen() {
+fn term_refresh() {
     let mut out = io::stdout();
     let out = out.by_ref();
     out.write(b"\x1b[2J").unwrap();
     out.write(b"\x1b[H").unwrap();
 }
 
+fn editor_refresh_screen(cfg: &EditorConfig) {
+    let mut out = io::stdout();
+    let out = out.by_ref();
+    out.write(b"\x1b[2J").unwrap();
+    out.write(b"\x1b[H").unwrap();
+
+    editor_draw_rows(cfg);
+
+    out.write(b"\x1b[H").unwrap();
+}
+
+fn editor_draw_rows(cfg: &EditorConfig) {
+    for _ in 0..cfg.screenrows {
+        io::stdout().write(b"~\r\n").unwrap();
+    }
+}
+
 fn main() {
-    let raw = enable_raw_mode().unwrap();
+    let cfg = EditorConfig::default();
+    enable_raw_mode(&cfg).unwrap();
     loop {
-        editor_refresh_screen();
-        editor_process_key(&raw);
+        editor_refresh_screen(&cfg);
+        editor_process_key(&cfg);
     }
 }
